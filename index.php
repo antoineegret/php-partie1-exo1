2cfc32f891583c25e4bd3cc0e9ad9cbbaf5c22b4
antoineegret <a.egret@laposte.net>
2019-12-05 14:27:34 +0100

    partie 1 exo 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exo1</title>
+</head>
+<body>
+    <h1>exercie 1 & 6</h1>
+    <p>Bonjour
+    <?php  
+    $name = "antoine";
+    echo $name;
+    ?>
+    , comment vas tu?</p>
+</body>
+</html>
